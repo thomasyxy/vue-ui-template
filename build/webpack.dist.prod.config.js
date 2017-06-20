275ffeca21cc8ac9bf1644b1bfad5bb0b916ebd7
yinxueyuan <838476901@qq.com>
2017-06-20 19:21:57 +0800

    fix: webpack config formate

--- build/webpack.dist.prod.config.js
+++ build/webpack.dist.prod.config.js
@@ -6,34 +6,34 @@ var webpackBaseConfig = require('./webpack.base.config.js');
 process.env.NODE_ENV = 'production';
 
 module.exports = merge(webpackBaseConfig, {
-    entry: {
-        main: './src/index.js'
-    },
-    output: {
-        path: path.resolve(__dirname, '../dist'),
-        publicPath: '/dist/',
-        filename: 'lemo.min.js',
-        library: 'lemo',
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
-    externals: {
-        vue: {
-            root: 'Vue',
-            commonjs: 'vue',
-            commonjs2: 'vue',
-            amd: 'vue'
-        }
-    },
-    plugins: [
-        // @todo
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': '"production"'
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        })
-    ]
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/dist/',
+    filename: 'lemo.min.js',
+    library: 'lemo',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  externals: {
+    vue: {
+      root: 'Vue',
+      commonjs: 'vue',
+      commonjs2: 'vue',
+      amd: 'vue'
+    }
+  },
+  plugins: [
+    // @todo
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
 });
--- build/webpack.test.config.js
+++ build/webpack.test.config.js
@@ -9,14 +9,14 @@ var webpackBaseConfig = require('./webpack.base.config.js');
 
 var webpackConfig = merge(webpackBaseConfig, {
   // use inline sourcemap for karma-sourcemap-loader
-    devtool: '#inline-source-map',
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"testing"'
-            }
-        })
-    ]
+  devtool: '#inline-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"testing"'
+      }
+    })
+  ]
 });
 
 // no need for app entry during tests
