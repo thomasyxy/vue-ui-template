f7249894cd8994ef4c9f195107558c9279bf0a23
yinxueyuan <838476901@qq.com>
2017-06-20 19:02:18 +0800

    feat: example

---
+++ examples/routers/button.vue
@@ -0,0 +1,18 @@
+<style>
+.demo-button {
+  width: 100px;
+  height: 30px;
+  background: #f60;
+  color: #fff
+}
+</style>
+<template>
+  <Button>
+    <span class="demo-button">按钮</span>
+  </Button>
+</template>
+<script>
+export default {
+
+}
+</script>
